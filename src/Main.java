0038477c334433707a7a31566445367de9cffeb8
shubh <shubhvcs@gmail.com>
2024-03-14 12:38:12 +0530

    [isPrime]

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
+    static Scanner ref = new Scanner(System.in);
+    static void isPrime(int number){
+        int count = 0;
+        for (int i = 1; i <= number; i++){
+            if(number%i == 0){
+                count++;
+            }
+        }
+        if(count == 2){
+            System.out.println("Prime Number");
+        }else{
+            System.out.println("Not Prime Number");
+        }
+    }
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+//        System.out.println("Hello and welcome!");
+
+        int num = ref.nextInt();
+        isPrime(num);
     }
 }
