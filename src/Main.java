e8ebb79ecbb0e1abba02d62b45909c5d42ea4def
shubh <shubhvcs@gmail.com>
2024-03-14 13:26:26 +0530

    [fix]

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
-import java.util.Scanner;
+import java.util.Arrays;
+//import java.util.Scanner;
 public class Main {
-    static Scanner ref = new Scanner(System.in);
+//    static Scanner ref = new Scanner(System.in);
     static void isPrime(int number){
         int count = 0;
         for (int i = 1; i <= number; i++){
@@ -19,7 +20,9 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
 //        System.out.println("Hello and welcome!");
 
-        int num = ref.nextInt();
-        isPrime(num);
+//        int num = ref.nextInt();
+//        for(int i = 0; i <= args.length();i ++)
+        System.out.println(Arrays.toString(args));
+//        isPrime();
     }
 }
